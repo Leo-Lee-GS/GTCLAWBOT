e9168dbd49a71f93c233abdbf5bb84d7c510659f
Leo-Lee-GS <swlee@gscaltex.com>
2024-10-18 11:01:50 +0900

    Update Pages_CHAT2.py

--- Pages_CHAT2.py
+++ Pages_CHAT2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # 챗봇을 화면 중앙에 배치
-st.subheader("저는 :blue[GT&C] 관련 :red[Q&A봇]입니다. 궁금한 사례를 물어보세요! :sunglasses:", divider="blue")
+st.subheader("저는 :blue[GT&C] 관련 :red[Q&A봇]입니다. 궁금한 사례를 물어보세요! :sunglasses:", divider="grey")
 # st.subheader("먼저 질문할 회사의 GTC를 선택하세요!", divider="gray")
 
 # iframe을 전체 화면 너비로 설정
