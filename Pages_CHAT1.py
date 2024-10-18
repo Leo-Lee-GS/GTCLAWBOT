85a1ad10d1c6827d679977ccd8f2c6ddbbce26e2
Leo-Lee-GS <swlee@gscaltex.com>
2024-10-18 10:56:19 +0900

    Update Pages_CHAT1.py

--- Pages_CHAT1.py
+++ Pages_CHAT1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 챗봇을 화면 중앙에 배치
-st.header("저는:blue[GT&C] 관련 :red[학습봇]입니다. 질문을 통해 쉽게 배워보세요! :sunglasses:", divider="blue")
+st.subheader("저는:blue[GT&C] 관련 :red[학습봇]입니다. 질문을 통해 쉽게 배워보세요! :sunglasses:", divider="blue")
 # st.subheader("먼저 질문할 회사의 GTC를 선택하세요!", divider="gray")
 st.components.v1.html(
     """
